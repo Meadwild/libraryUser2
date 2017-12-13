0b5e1c58774b55d94282cb17b8875ea2da83e588
Rye_eyes <ryeyes@mail.ru>
2017-12-13 12:22:06 +0300

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Дмитрий on 13.12.2017.
+ */
+
+
+import org.kohsuke.github.GitHubBuilder;
+
+public class Main {
+
+    static void main(String[] args ){
+        System.out.println("Hello, world.");
+    }
+}
